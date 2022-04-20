e806a33ec94c16f0839f79d5977af28e1b4abb8c
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-04-20 23:40:02 +0600

    object as api create, dynamic parameter user find

--- index.js
+++ index.js
@@ -8,6 +8,37 @@ app.get('/',(req,res)=>{
 
 
 
+/* app.get('/users', (req, res)=>{
+    res.send('we found users');
+}) */
+
+// object send basic style 
+
+/* app.get('/users', (req, res)=>{
+    res.send({id:1, name: 'pias sarkar', job: 'shudu ghumai '});
+}) */
+
+
+const users =[
+    {id:1 , name :'manik bhai', email:"maink@gmail.com", phone:"0924242323"},
+    {id:2 , name :'zubayerbhai', email:"zubayer@gmail.com", phone:"0924242323"},
+    {id:3 , name :'mamun bhai', email:"mamun@gmail.com", phone:"0924242323"},
+    {id:4 , name :'tommoy bhai', email:"tommoy@gmail.com", phone:"0924242323"},
+    {id:5 , name :'farid bhai', email:"farid@gmail.com", phone:"0924242323"},
+    {id:6 , name :'sayem bhai', email:"sayem@gmail.com", phone:"0924242323"},
+]
+
+app.get('/users', (req,res)=>{
+    res.send(users);
+})
+
+
+app.get('/user/:id',(req,res)=>{
+    // console.log(req.params);
+    const id = req.params.id;
+    const user = users.find(user=> user.id==id);
+    res.send(user);
+})
 /* listen port as a port your set */
 app.listen(port,()=>{
     console.log(`port running on ${port}`);
