6995813893d4c2cb2dae1294247e11303ff09d05
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-04-21 00:35:16 +0600

    post data store in server array and then send as a get request

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT ||5000;
+const port = process.env.PORT || 5000;
 const cors = require('cors');
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('look this express for server make by zaman');
 })
 
@@ -22,37 +22,39 @@ app.use(express.json());
 }) */
 
 
-const users =[
-    {id:1 , name :'manik bhai', email:"maink@gmail.com", phone:"0924242323"},
-    {id:2 , name :'zubayerbhai', email:"zubayer@gmail.com", phone:"0924242323"},
-    {id:3 , name :'mamun bhai', email:"mamun@gmail.com", phone:"0924242323"},
-    {id:4 , name :'tommoy bhai', email:"tommoy@gmail.com", phone:"0924242323"},
-    {id:5 , name :'farid bhai', email:"farid@gmail.com", phone:"0924242323"},
-    {id:6 , name :'sayem bhai', email:"sayem@gmail.com", phone:"0924242323"},
+const users = [
+    { id: 1, name: 'manik bhai', email: "maink@gmail.com", phone: "0924242323" },
+    { id: 2, name: 'zubayerbhai', email: "zubayer@gmail.com", phone: "0924242323" },
+    { id: 3, name: 'mamun bhai', email: "mamun@gmail.com", phone: "0924242323" },
+    { id: 4, name: 'tommoy bhai', email: "tommoy@gmail.com", phone: "0924242323" },
+    { id: 5, name: 'farid bhai', email: "farid@gmail.com", phone: "0924242323" },
+    { id: 6, name: 'sayem bhai', email: "sayem@gmail.com", phone: "0924242323" },
 ]
 
-app.get('/users', (req,res)=>{
+app.get('/users', (req, res) => {
     res.send(users);
 })
 
 
-app.get('/user/:id',(req,res)=>{
+app.get('/user/:id', (req, res) => {
     // console.log(req.params);
     const id = req.params.id;
-    const user = users.find(user=> user.id==id);
+    const user = users.find(user => user.id == id);
     res.send(user);
 })
 
 /* post req and respond */
 
-app.post('/user', (req, res)=>{
-    console.log(req.body);
-    res.send('post method seccess');
+app.post('/user', (req, res) => {
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    res.send(user);
 })
 
 
 
 /* listen port as a port your set */
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`port running on ${port}`);
 })
