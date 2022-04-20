e5aa8619f7decf5c077b8260fc7e6120d7354ef7
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-04-20 23:14:50 +0600

    how to install,init and run and get request frombrowser

---
+++ _step.js
@@ -0,0 +1,16 @@
+/***
+ * 1. create a floder (manually or mkdir)
+ * 2. open command linke to that floder 
+ * 3.npm init
+ * 4. npm install express 
+ *  --------------------------------
+ * for Express/ node server 
+ * ----------------------------------
+ * 1.require express
+ * 2.create app variable
+ * 3. declare port 
+ * 4.set app.set('/')
+ * 5.listen to port 
+ * 6.node index.js(as you start file name)
+ * 7.check you borwer for that port
+ */ 
